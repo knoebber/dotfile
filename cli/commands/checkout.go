e417141cf718c80580787facbb0159a08d061f0c
Alex Mitchell <alex@alexthemitchell.com>
2019-05-11 11:20:22 -0700

    Fill out commands

---
+++ cli/commands/checkout.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"fmt"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+type checkoutCommand struct {
+	fileName   string
+	commitHash string
+}
+
+func (c *checkoutCommand) run(ctx *kingpin.ParseContext) error {
+	fmt.Printf("TODO: Checkout %#v commitHash: %#v\n", c.fileName, c.commitHash)
+	return nil
+}
+
+func addCheckoutSubCommandToApplication(app *kingpin.Application) {
+	cc := &checkoutCommand{}
+	c := app.Command("checkout", "revert a file to a previously committed state").Action(cc.run)
+	c.Arg("file-name", "file to check changes in").Required().StringVar(&cc.fileName)
+	c.Arg("commit-hash", "the revision to revert to").StringVar(&cc.commitHash)
+}
