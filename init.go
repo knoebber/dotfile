ccb0a289dffeae28d2e4ea53854182a9ab6dd9eb
Nicolas Knoebber <knoebber@gmail.com>
2019-05-11 21:25:30 -0500

    Get path working

--- init.go
+++ init.go
@@ -1,6 +1,7 @@
 package dotfile
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,6 +12,11 @@ func Init(path string, altName string) error {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return fmt.Errorf("%s not found", path)
 	}
+
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
 	if altName == "" {
 		// Create a name from the path of the file.
 		// Examples: ~/.vimrc: vimrc
@@ -20,7 +26,13 @@ func Init(path string, altName string) error {
 		altName = re.FindStringSubmatch(path)[1]
 	}
 
-	path = filepath.Dir(path)
+	// Replace the full path with a relative path
+	home := os.Getenv("HOME")
+	if home == "" {
+		return errors.New("HOME environment variable must be set")
+	}
+	re := regexp.MustCompile(home)
+	path = re.ReplaceAllString(path, "~")
 
 	fmt.Printf("Initialized %s as %s\n", path, altName)
 	return nil
