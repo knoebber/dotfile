789d744dc9edeaf3fd60ce1a2abc1b897aa1adaa
Nicolas Knoebber <knoebber@gmail.com>
2020-04-11 17:30:55 -0500

    Ensure testdata/ is created before test

--- db/initialize_test.go
+++ db/initialize_test.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStart(t *testing.T) {
+	os.Mkdir("testdata/", 0755)
+
 	assert.NoError(t, Start("testdata/dotfilehub.db"))
 }
