34ee5ccd14891ff151e5cf823a50a035098ae11c
Nicolas Knoebber <knoebber@gmail.com>
2020-04-13 22:28:43 -0500

    Add unique index on user_id and alias
    This will allow routes like /knoebber/bashrc to work without having to
    worry about duplicates.

--- db/file.go
+++ db/file.go
@@ -3,23 +3,24 @@ package db
 // File models the files table.
 // It stores the contents of a file at the current revision hash.
 type File struct {
-	ID       int
-	UserID   int    `validate:"required"`
-	Alias    string `validate:"required"`
-	Path     string `validate:"required"`
-	Current  string `validate:"required"`
-	Contents []byte `validate:"required"`
+	ID      int
+	UserID  int    `validate:"required"`
+	Alias   string `validate:"required"`
+	Path    string `validate:"required"`
+	Current string `validate:"required"`
+	Content []byte `validate:"required"`
 }
 
 func (*File) createStmt() string {
 	return `
 CREATE TABLE IF NOT EXISTS files(
-id                   INTEGER PRIMARY KEY,
-user_id              INTEGER NOT NULL REFERENCES users,
-alias                TEXT NOT NULL,
-path                 TEXT NOT NULL,
-currents             TEXT NOT NULL,
-content              BLOB NOT NULL
+id       INTEGER PRIMARY KEY,
+user_id  INTEGER NOT NULL REFERENCES users,
+alias    TEXT NOT NULL,
+path     TEXT NOT NULL,
+current  TEXT NOT NULL,
+content  BLOB NOT NULL
 );
-CREATE INDEX IF NOT EXISTS files_user_index ON files(user_id);`
+CREATE INDEX IF NOT EXISTS files_user_index ON files(user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS files_user_alias_index ON files(user_id, alias);`
 }
