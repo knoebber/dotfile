4fba0c983abfa011b8ccbc17dc6d9da51e489346
Nicolas Knoebber <knoebber@gmail.com>
2020-10-15 22:07:02 -0700

    Explicitly ignore errors, remove terms link, minor cleanups

--- db/html_table.go
+++ db/html_table.go
@@ -46,15 +46,15 @@ func (h *HTMLTable) Header() template.HTML {
 
 	for i, col := range h.Columns {
 		query, curr := h.Controls.orderCol(i + 1)
-		b.WriteString("<th>")
+		_, _ = b.WriteString("<th>")
 		if curr == "asc" {
-			b.WriteString(aTag(query, htmlUpArrow+col))
+			_, _ = b.WriteString(aTag(query, htmlUpArrow+col))
 		} else if curr == "desc" {
-			b.WriteString(aTag(query, htmlDownArrow+col))
+			_, _ = b.WriteString(aTag(query, htmlDownArrow+col))
 		} else {
-			b.WriteString(aTag(query, col))
+			_, _ = b.WriteString(aTag(query, col))
 		}
-		b.WriteString("</th>")
+		_, _ = b.WriteString("</th>")
 	}
 	return template.HTML(b.String())
 }
@@ -70,15 +70,15 @@ func (h *HTMLTable) Pages() template.HTML {
 		return ""
 	}
 
-	b.WriteString(fmt.Sprintf("<i>%d of %d results</i>", len(h.Rows), h.Controls.totalRows))
-	b.WriteString(htmlSpace)
+	_, _ = b.WriteString(fmt.Sprintf("<i>%d of %d results</i>", len(h.Rows), h.Controls.totalRows))
+	_, _ = b.WriteString(htmlSpace)
 
 	if curr > 1 {
 		// Write the first page and the previous page.
-		b.WriteString(aTag(h.Controls.previousPage(), htmlLeftArrow+" previous"))
-		b.WriteString(htmlCommaSpace)
-		b.WriteString(aTag(h.Controls.firstPage(), "1"))
-		b.WriteString(htmlCommaSpace)
+		_, _ = b.WriteString(aTag(h.Controls.previousPage(), htmlLeftArrow+" previous"))
+		_, _ = b.WriteString(htmlCommaSpace)
+		_, _ = b.WriteString(aTag(h.Controls.firstPage(), "1"))
+		_, _ = b.WriteString(htmlCommaSpace)
 	}
 
 	for p := curr - pagesAround; p < curr; p++ {
@@ -86,27 +86,27 @@ func (h *HTMLTable) Pages() template.HTML {
 			continue
 		}
 		// Write the pages before the current.
-		b.WriteString(aTag(h.Controls.toPage(p), strconv.Itoa(p)))
-		b.WriteString(htmlCommaSpace)
+		_, _ = b.WriteString(aTag(h.Controls.toPage(p), strconv.Itoa(p)))
+		_, _ = b.WriteString(htmlCommaSpace)
 	}
 
 	// Write the current page.
-	b.WriteString("<strong>")
-	b.WriteString(strconv.Itoa(curr))
-	b.WriteString("</strong>")
+	_, _ = b.WriteString("<strong>")
+	_, _ = b.WriteString(strconv.Itoa(curr))
+	_, _ = b.WriteString("</strong>")
 
 	for p := curr + 1; p < total && p < curr+pagesAround; p++ {
 		// Write the pages after current.
-		b.WriteString(htmlCommaSpace)
-		b.WriteString(aTag(h.Controls.toPage(p), strconv.Itoa(p)))
+		_, _ = b.WriteString(htmlCommaSpace)
+		_, _ = b.WriteString(aTag(h.Controls.toPage(p), strconv.Itoa(p)))
 	}
 
 	if curr < total {
 		// Write the last page and the next page.
-		b.WriteString(htmlCommaSpace)
-		b.WriteString(aTag(h.Controls.lastPage(), strconv.Itoa(total)))
-		b.WriteString(htmlSpace)
-		b.WriteString(aTag(h.Controls.nextPage(), "next "+htmlRightArrow))
+		_, _ = b.WriteString(htmlCommaSpace)
+		_, _ = b.WriteString(aTag(h.Controls.lastPage(), strconv.Itoa(total)))
+		_, _ = b.WriteString(htmlSpace)
+		_, _ = b.WriteString(aTag(h.Controls.nextPage(), "next "+htmlRightArrow))
 	}
 	return template.HTML(b.String())
 }
