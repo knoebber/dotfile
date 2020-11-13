f70162605e1b2d37aa586a9982fbaa7df5eefcba
Riley Quinn <merileyquinn@gmail.com>
2020-11-13 12:11:04 -0600

    Error if file with uncommitted changes is checked out, added -f to checkout

--- cli/checkout.go
+++ cli/checkout.go
@@ -1,13 +1,16 @@
 package cli
 
 import (
+	"fmt"
 	"github.com/knoebber/dotfile/dotfile"
+	"github.com/knoebber/dotfile/usererror"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type checkoutCommand struct {
 	alias      string
 	commitHash string
+	force      bool
 }
 
 func (c *checkoutCommand) run(*kingpin.ParseContext) error {
@@ -17,6 +20,16 @@ func (c *checkoutCommand) run(*kingpin.ParseContext) error {
 	}
 	if c.commitHash == "" {
 		c.commitHash = s.FileData.Revision
+	}
+
+	if !c.force {
+		clean, err := dotfile.IsClean(s, c.commitHash)
+		if err != nil {
+			return err
+		}
+		if !clean {
+			return usererror.Invalid(fmt.Sprintf(`"%s" has uncommitted changes, use -f to override`, c.alias))
+		}
 	}
 
 	if err := dotfile.Checkout(s, c.commitHash); err != nil {
@@ -32,4 +45,5 @@ func addCheckoutSubCommandToApplication(app *kingpin.Application) {
 	c := app.Command("checkout", "revert a file to a previously committed state").Action(cc.run)
 	c.Arg("alias", "name of file to revert changes in").Required().StringVar(&cc.alias)
 	c.Arg("commit-hash", "the revision to revert to").StringVar(&cc.commitHash)
+	c.Flag("force", "revert a file with uncommitted changes").Short('f').BoolVar(&cc.force)
 }
